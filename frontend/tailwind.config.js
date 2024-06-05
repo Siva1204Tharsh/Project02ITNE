eb462584d7335b4c701a89b56324562ed4e10a22
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-06-05 16:00:26 +0530

    one

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./node_modules/tw-elements/dist/js/**/*.js",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [require("tw-elements/dist/plugin")];
